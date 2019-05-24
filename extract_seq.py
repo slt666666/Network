c57c5b460f65fe15bae53b1ee8379e6b9b68859c
堺俊之 <toshi6661024@gmail.com>
2019-05-24 10:55:15 +0100

    add coffea data

--- extract_seq.py
+++ extract_seq.py
@@ -4,18 +4,35 @@ import sys
 from Bio import SeqIO
 
 
-tomato_NLRs = pd.read_csv("original_data/NLR_gene_info.csv")
+# ### extract fasta of NLR ids
+# coffea_NLRs = pd.read_csv("Coffee/original_data/NLR_gene_info.csv")
+#
+# with open("Coffee/original_data/coffea_NLR_seq.fasta", mode='w') as f:
+#
+#     for record in SeqIO.parse('Coffee/original_data/coffea_pep.faa', 'fasta'):
+#         id_part = record.id
+#         desc_part = record.description
+#         seq = record.seq
+#
+#         if id_part[:14] in coffea_NLRs["gene_id"].tolist():
+#             print(id_part[:14])
+#             f.write(">"+id_part[:14])
+#             f.write("\n")
+#             f.write(str(record.seq))
+#             f.write("\n")
 
-with open("original_data/tomato_NLR_seq.fasta", mode='w') as f:
 
-    for record in SeqIO.parse('original_data/ITAG3.2_proteins.fasta', 'fasta'):
+### cut based on K of GKTT
+with open("Coffee/original_data/coffee_alignment_BN_extractAtZAR1_cutK.fasta", mode='w') as f:
+
+    for record in SeqIO.parse('Coffee/original_data/coffee_alignment_BN_extractAtZAR1.fasta', 'fasta'):
         id_part = record.id
         desc_part = record.description
         seq = record.seq
-
-        if id_part[:14] in tomato_NLRs["gene_id"].tolist():
-            print(id_part[:14])
-            f.write(">"+id_part[:14])
+        if str(record.seq)[50] == "K":
+            # print(id_part)
+            # print(str(record.seq)[49:53])
+            f.write(">"+id_part)
             f.write("\n")
             f.write(str(record.seq))
             f.write("\n")
